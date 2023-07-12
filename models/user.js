d90cec9b9b0d1896e323e8e6bcbd6c4ee4ced85c
Hannes Porombka <hapo@Lutz-MacBook-pro-15.local>
2023-07-13 01:58:08 +0200

    user register route working

--- models/user.js
+++ models/user.js
@@ -13,9 +13,10 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { 
-    const hashedPw = await bcrypt.hash(password, BCRYPT_WORK_FACTOR )
-    const join_at = Date.now()
+  static async register(username, password, first_name, last_name, phone) { 
+    let hashedPw = await bcrypt.hash(password, 10)
+    console.log(hashedPw)
+    const join_at = new Date(Date.now())
     const last_login_at = join_at
     console.log(hashedPw)
     const result = await db.query("INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING username, password, first_name, last_name, phone", [username, hashedPw, first_name, last_name, phone, join_at, last_login_at])
@@ -72,7 +73,11 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const results = await db.query ("SELECT id, to_username, body, sent_at, read_at FROM messages WHERE from_username = $1", [username])
+    results.rows.forEach(async d => d.to_username = await db.query ("SELECT username, first_name, last_name, phone FROM users WHERE username =$1" [d.to_username]))
+    return results.rows
+  }
 
   /** Return messages to this user.
    *
@@ -82,7 +87,11 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) { 
+    const results = await db.query ("SELECT id, from_username, body, sent_at, read_at FROM messages WHERE to_username = $1", [username])
+    results.rows.forEach(async d => d.from_username = await db.query ("SELECT username, first_name, last_name, phone FROM users WHERE username =$1" [d.from_username]))
+    return results.rows
+  }
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -11,3 +11,63 @@
  *
  *  Make sure to update their last-login!
  */
+
+
+const express = require("express");
+const jwt = require("jsonwebtoken")
+const { SECRET_KEY } = require("../config")
+
+const expressError = require ("../expressError")
+
+const User = require("../models/user");
+
+const router = new express.Router();
+
+router.post("/register/", async function(req,res, next){
+    try {
+        const {username, password, first_name, last_name, phone} = req.body
+        if (!username || !password || !first_name || !last_name || !phone) {
+            throw new expressError("Please enter username, password, first name, last name and phone!", 400)
+        }
+        console.log(username)
+        const result = await User.register(username, password, first_name, last_name, phone)
+        if (!result){
+            throw new expressError("Error Registering!", 400)
+        }
+        return res.json({"message": "Registered!"})
+
+
+    }
+    catch(e){
+        return next(e)
+
+    }
+
+})
+
+
+router.post("/login/", async function(req,res, next){
+    try {
+        const {username, password} = req.body
+        if (!username || !password) {
+            throw new expressError("Please enter username and password!", 400)
+        }
+        const result = User.authenticate(username, password)
+        if (!result){
+            throw new expressError("False credentials!", 400)
+        }
+        const token = jwt.sign({username}, SECRET_KEY)
+        User.updateLoginTimestamp(username)
+        return res.json({token})
+
+
+    }
+    catch(e){
+        return next(e)
+
+    }
+
+})
+
+
+module.exports = router;
